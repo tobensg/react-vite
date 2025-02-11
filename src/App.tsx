5b8a62df079027fad45304e1c8762967dd231cf0
TOBEN GREEN <toben@TobenHomeBook.local>
2025-02-11 11:33:46 -0800

    first push

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div className='card'>
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+
+        <p class='mb-3 text-gray-500 dark:text-gray-400'>
+          Basic react app installation using vite
+        </p>
+        <p class='text-gray-500 dark:text-gray-400'>
+          With a quick add on of tailwind for some easy css
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default App;
